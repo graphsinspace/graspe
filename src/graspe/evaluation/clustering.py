86d8d01d63d8771456ab856c239e5509f02967a9
LucyK243 <lucy@dmi.uns.ac.rs>
2021-01-21 15:34:17 +0100

    added more clustering methods

--- src/graspe/evaluation/clustering.py
+++ src/graspe/evaluation/clustering.py
@@ -1,6 +1,8 @@
 from embeddings.base.embedding import Embedding
 from common.graph import Graph
 from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.cluster import completeness_score
 from sklearn.metrics.cluster import adjusted_mutual_info_score
 from sklearn.metrics.cluster import adjusted_rand_score
@@ -16,7 +18,8 @@ class ClusteringEval():
     def __init__(
         self,
         graph,
-        embedding
+        embedding,
+        clustering_type
     ):
         self._graph = graph
         self._embedding = embedding
@@ -31,13 +34,14 @@ class ClusteringEval():
         self._normalized_mutual_info_score = 0.0
         self._rand_score = 0.0
         self._v_measure_score = 0.0
-        self.__evaluate()
+        self.__evaluate(clustering_type)
 
 
-    def __evaluate(self):
+    def __evaluate(self, clustering_type):
         no_clusters = len(set(self._labels))
-        km = KMeans(no_clusters)
-        clusters = km.fit_predict(self._node_vectors)
+        function = self.__get_clustering_function(clustering_type)
+        clustering = function(no_clusters)
+        clusters = clustering.fit_predict(self._node_vectors)
         self._ajusted_mutual_info_score = adjusted_mutual_info_score(self._labels, clusters)
         self._adjusted_rand_score = adjusted_rand_score(self._labels, clusters)
         self._completeness_score = completeness_score(self._labels, clusters)
@@ -48,6 +52,14 @@ class ClusteringEval():
         self._rand_score = rand_score(self._labels, clusters)
         self._v_measure_score = v_measure_score(self._labels, clusters)
 
+    def __get_clustering_function(self, clustering_type):
+        cltype = clustering_type.lower()
+        if(cltype == "agglomerative"):
+            return AgglomerativeClustering
+        elif(cltype == "kmeans"):
+            return KMeans
+        else:
+            return SpectralClustering
 
     def get_adjusted_mutual_info_score(self):
         return self._ajusted_mutual_info_score
--- src/graspe/test_clustering.py
+++ src/graspe/test_clustering.py
@@ -2,11 +2,11 @@ from common.dataset_pool import DatasetPool
 from embeddings.embedding_node2vec import Node2VecEmbedding
 from evaluation.clustering import ClusteringEval
 
-graph = DatasetPool.load("cora_ml")
+graph = DatasetPool.load("karate_club_graph")
 embedding = Node2VecEmbedding(graph, 10, 0.1, 0.5)
 embedding.embed()
 
-eval = ClusteringEval(graph, embedding)
+eval = ClusteringEval(graph, embedding, 'aGglomerative')
 print("adjMI = ", eval.get_adjusted_mutual_info_score())
 print("adjRand = ", eval.get_adjusted_rand_score())
 print("fm = ", eval.get_fowlkes_mallows_score())
