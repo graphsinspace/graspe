509bd5a68d2920b53eee0bf428b2247e01a16ce1
nmilosev <nmilosevnm@gmail.com>
2021-02-05 11:29:19 +0100

    Fix for loading sparse  matrices to pytorch_geo
    - GAE with cora_ml now works

--- src/graspe/tests/test_gae.py
+++ src/graspe/tests/test_gae.py
@@ -38,8 +38,18 @@ def test_gae_variational_linear():
         print(i, gae_embedding[i])
 
 
+def test_gae_cora():
+    g = DatasetPool.load("cora_ml")
+    gae_embedding = GAEEmbedding(g, d=10, epochs=5, variational=False, linear=False)
+    gae_embedding.embed()
+    assert gae_embedding._embedding is not None
+    for i in range(34):
+        print(i, gae_embedding[i])
+
+
 if __name__ == "__main__":
     test_gae_variational()
     test_gae_normal()
     test_gae_normal_linear()
     test_gae_variational_linear()
+    test_gae_cora()
