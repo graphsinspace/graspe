792dc1ac459e36cd3a01c09cb056ad65893a0bac
Nemanja Milosevic <nmilosevnm@gmail.com>
2021-02-09 17:25:20 +0100

    GCN fixes
    - Fix for graphs with isolated nodes:
    
        dgl._ffi.base.DGLError: There are 0-in-degree nodes in the graph, output
        for those nodes will be invalid. This is harmful for some applications,
        causing silent performance regression. Adding self-loop on the input
        graph by calling ±g = dgl.add_self_loop(g)± will resolve the issue.
        Setting ±±allow_zero_in_degree±± to be ±True± when constructing this
        module will suppress the check and let the code run.
    
    - Added add_self_loop parameter to embedding constructor to overcome this problem.
    - Added more GCN tests (for all datasets)

--- src/graspe/tests/test_gcn.py
+++ src/graspe/tests/test_gcn.py
@@ -11,5 +11,20 @@ def test_gcn_citeseer():
         print(i, e[i])
 
 
+def test_gcn_all():
+    datasets = DatasetPool.get_datasets()
+    needs_self_loop = ["amazon_electronics_computers", "amazon_electronics_photo"]
+    print(datasets)
+    for dataset_name in datasets:
+        if dataset_name in ["davis_southern_women_graph", "florentine_families_graph", "les_miserables_graph"]:
+            continue
+        print(dataset_name)
+        g = DatasetPool.load(dataset_name)
+        e = GCNEmbedding(g, d=1, epochs=1, add_self_loop=dataset_name in needs_self_loop)
+        e.embed()
+        assert e._embedding is not None
+
+
 if __name__ == "__main__":
     test_gcn_citeseer()
+    test_gcn_all()
