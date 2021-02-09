9ba6643f0a6d7c9cfd75647bccc9295fd9a14a9a
Dusan Stamenkovic <dusan.v.stamenkovic@icloud.com>
2021-02-09 11:03:27 +0100

    supervised prediction of links - small update

--- src/graspe/test_supervised_link.py
+++ src/graspe/test_supervised_link.py
@@ -1,7 +1,7 @@
 from common.dataset_pool import DatasetPool
 from embeddings.embedding_node2vec import Node2VecEmbedding
 import numpy as np
-
+import time
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -12,37 +12,28 @@ from sklearn.metrics import (
 )
 
 if __name__ == '__main__':
-    graph = DatasetPool.load("amazon_electronics_photo")
+    graph = DatasetPool.load('florentine_families_graph')
     embedding = Node2VecEmbedding(graph, 10, 0.1, 0.5)
+    start_time = time.time()
     embedding.embed()
+    print('{}s needed for embedding of the graph'.format(time.time() - start_time))
 
     x = []
     y = []
 
     nodes = list(embedding._embedding.keys())
+    print('num nodes: ', len(nodes))
 
     for i in range(len(nodes)):
         node1 = nodes[i]
         for j in range(i + 1, len(nodes)):
             node2 = nodes[j]
             x.append(np.concatenate([embedding._embedding[node1], embedding._embedding[node2]], axis=-1))
-            y.append(1 if j in graph.to_networkx().neighbors(i) else 0)
+            y.append(1 if node2 in graph.to_networkx().neighbors(node1) else 0)
     x = np.stack(x, axis=0)
     y = np.array(y)
 
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.33, random_state=42)
-
-    lr = LogisticRegression()
-    lr.fit(xtrain, ytrain)
-    predictions = lr.predict(xtest)
-
-    acc = accuracy_score(ytest, predictions)
-    prec = precision_score(ytest, predictions)
-    recall = recall_score(ytest, predictions)
-
-    print('LogisticRegression accuracy score : ', acc)
-    print('LogisticRegression precision score : ', prec)
-    print('LogisticRegression recall score : ', recall)
 
     rf = RandomForestClassifier(n_estimators=10)
     rf.fit(xtrain, ytrain)
