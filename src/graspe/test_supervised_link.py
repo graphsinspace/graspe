b1b448b3b05675d81ec46a2e149f980514dc2fae
Dusan Stamenkovic <dusan.v.stamenkovic@icloud.com>
2021-02-12 11:31:13 +0100

    balanced dataset

--- src/graspe/test_supervised_link.py
+++ src/graspe/test_supervised_link.py
@@ -9,8 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 if __name__ == "__main__":
-    percentages = np.linspace(0.1, 1, 10)
-    graph = DatasetPool.load("karate_club_graph")
+    graph = DatasetPool.load("cora_ml")
     embedding = Node2VecEmbedding(graph, 10, 0.1, 0.5)
     start_time = time.time()
     embedding.embed()
@@ -41,34 +40,31 @@ if __name__ == "__main__":
     x = np.stack(x, axis=0)
     y = np.array(y)
     print('number of positive samples in dataset : ', len(y[y == 1]))
-    num_negative = len(np.where(y == 0)[0])
-    for percentage in percentages:
-        # subsampling
-        neg_indices = np.where(y == 0)[0]  # indices of negative samples
-        np.random.shuffle(neg_indices)  # shuffle (inplace)
-        neg_indices_sub = neg_indices[:int(percentage * num_negative)]  # subsample
-        pos_indices = np.where(y == 1)[0]  # indices of positive samples
-        indices = np.concatenate([neg_indices_sub, pos_indices], axis=-1)
-        np.random.shuffle(indices)  # shuffle (inplace)
-        x_sub = x[indices]
-        y_sub = y[indices]
-        neg = len(y_sub[y_sub == 0]) / len(y_sub)
-        print('number of samples in dataset @ {}% of negative class : '.format(percentage*100), len(x_sub))
-        print('percentage of negative class in formed dataset @ {}% of negative class : '.format(percentage*100), neg)
-        xtrain, xtest, ytrain, ytest = train_test_split(
-            x_sub, y_sub, test_size=0.33, random_state=42
-        )
+    pos_indices = np.where(y == 1)[0]  # indices of positive samples
+    num_positive = len(pos_indices)
 
-        rf = RandomForestClassifier(n_estimators=10)
-        rf.fit(xtrain, ytrain)
-        predictions = rf.predict(xtest)
+    # subsampling
+    neg_indices = np.where(y == 0)[0]  # indices of negative samples
+    np.random.shuffle(neg_indices)  # shuffle (inplace)
+    neg_indices_sub = neg_indices[:int(num_positive)]  # subsample
+    indices = np.concatenate([neg_indices_sub, pos_indices], axis=-1)
+    np.random.shuffle(indices)  # shuffle (inplace)
+    x_sub = x[indices]
+    y_sub = y[indices]
+    print('percentage of negative class in subsampled dataset : ', len(y_sub[y_sub == 0]) / len(y_sub))
 
-        acc = accuracy_score(ytest, predictions)
-        prec = precision_score(ytest, predictions)
-        recall = recall_score(ytest, predictions)
+    xtrain, xtest, ytrain, ytest = train_test_split(
+        x_sub, y_sub, test_size=0.33, random_state=42
+    )
 
-        print('RandomForestClassifier accuracy score @ {}% of negative class : '.format(percentage*100), acc)
-        print('RandomForestClassifier precision score @ {}% of negative class : '.format(percentage*100), prec)
-        print('RandomForestClassifier recall score @ {}% of negative class : '.format(percentage*100), recall)
-        print('\n')
+    rf = RandomForestClassifier(n_estimators=10)
+    rf.fit(xtrain, ytrain)
+    predictions = rf.predict(xtest)
 
+    acc = accuracy_score(ytest, predictions)
+    prec = precision_score(ytest, predictions)
+    recall = recall_score(ytest, predictions)
+
+    print('RandomForestClassifier accuracy score : ', acc)
+    print('RandomForestClassifier precision score : ', prec)
+    print('RandomForestClassifier recall score : ', recall)
