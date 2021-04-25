bfa1cf29c016ca01e400d27254fde5743890dc70
Dusan Stamenkovic <dusan.v.stamenkovic@icloud.com>
2021-04-25 20:55:00 +0200

    small fix

--- src/graspe/tests/test_rec_eval_gcn.py
+++ src/graspe/tests/test_rec_eval_gcn.py
@@ -5,14 +5,14 @@ from embeddings.embedding_gcn import GCNEmbedding
 def test_reconstuction_evaluation():
     dataset = 'cora_ml'
     with open('./{}_results.csv'.format(dataset), 'w') as file:
-        file.write('PRECISION@K,MAP,RECALL,F1\n')
+        file.write('PARAMS,PRECISION@K,MAP,RECALL,F1\n')
     # first case
     iter = 1
     for d in [10, 25, 50, 100, 200]:
-        for epochs in [10, 50, 100]:
+        for epochs in [10, 50, 100, 200]:
             for n_layers in [1, 2, 3, 4]:
                 for dropout in [0.0, 0.1, 0.2, 0.3]:
-                    print('...starting to work on iter {} out of {}... '.format(iter, 4*4*4*3)) 
+                    print('...starting to work on iter {} out of {}... '.format(iter, 4*4*4*3))
                     graph = DatasetPool.load(dataset).to_undirected()
                     emb_m = GCNEmbedding(graph, d=d, epochs=epochs, n_layers=n_layers, dropout=dropout)
                     emb_m.embed()
@@ -33,8 +33,16 @@ def test_reconstuction_evaluation():
                     f1_val = (2 * precision_val * recall_val) / (precision_val + recall_val)
                     print('PRECISION@K = ', precision_val, 'MAP = ', map_val,
                           ', RECALL = ', recall_val, ', F1 = ', f1_val)
-                    with open('./cora_ml_dir.csv', 'a') as file:
-                        file.write('{},{},{}.{}\n'.format(precision_val, map_val, recall_val, f1_val))
+                    with open('./{}_results.csv'.format(dataset), 'a') as file:
+                        file.write('d={}_epochs={}_n_layers={}_dropout={},{},{},{}.{}\n'.format(
+                            d,
+                            epochs,
+                            n_layers,
+                            dropout,
+                            precision_val,
+                            map_val,
+                            recall_val,
+                            f1_val))
 
                     iter += 1
 
