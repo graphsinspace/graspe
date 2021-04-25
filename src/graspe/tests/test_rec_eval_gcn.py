16996831a4c5d63f27d38c9768b65b57d09b4301
Dusan Stamenkovic <dusan.v.stamenkovic@icloud.com>
2021-04-25 10:04:25 +0200

    small update

--- src/graspe/tests/test_rec_eval_gcn.py
+++ src/graspe/tests/test_rec_eval_gcn.py
@@ -13,25 +13,28 @@ def test_reconstuction_evaluation():
         for epochs in [10, 50, 100]:
             for n_layers in [1, 2, 3, 4]:
                 for dropout in [0.0, 0.1, 0.2, 0.3]:
+                    print('...starting on {} out of {}... ')
                     graph = DatasetPool.load(dataset)
                     emb_m = GCNEmbedding(graph, d=d, epochs=epochs, n_layers=n_layers, dropout=dropout)
                     emb_m.embed()
 
                     undirected_projection = graph.to_undirected()
-                    print("#nodes = ", undirected_projection.nodes_cnt())
-                    print("#edges = ", undirected_projection.edges_cnt())
+                    # print("#nodes = ", undirected_projection.nodes_cnt())
+                    # print("#edges = ", undirected_projection.edges_cnt())
 
                     num_links = undirected_projection.edges_cnt()
 
                     reconstructed_graph = emb_m.reconstruct(num_links)
-                    print("#nodes = ", reconstructed_graph.nodes_cnt())
-                    print("#edges = ", reconstructed_graph.edges_cnt())
+                    # print("#nodes = ", reconstructed_graph.nodes_cnt())
+                    # print("#edges = ", reconstructed_graph.edges_cnt())
 
                     # graph reconstruction evaluation
                     precision_val = undirected_projection.link_precision(reconstructed_graph)
                     map_val, _ = undirected_projection.map_value(reconstructed_graph)
                     recall_val, _ = undirected_projection.recall(reconstructed_graph)
                     f1_val = (2 * precision_val * recall_val) / (precision_val + recall_val)
+                    print("PRECISION@K = ", precision_val, "MAP = ", map_val,
+                          ", RECALL = ", recall_val, ', F1 = ', f1_val)
                     with open('./cora_ml_undir.csv', 'a') as file:
                         file.write('{},{},{}.{}\n'.format(precision_val, map_val, recall_val, f1_val))
 
@@ -40,21 +43,22 @@ def test_reconstuction_evaluation():
                     emb_m = GCNEmbedding(graph, d=d, epochs=epochs, n_layers=n_layers, dropout=dropout)
                     emb_m.embed()
 
-                    print("#nodes = ", graph.nodes_cnt())
-                    print("#edges = ", graph.edges_cnt())
+                    # print("#nodes = ", graph.nodes_cnt())
+                    # print("#edges = ", graph.edges_cnt())
 
                     num_links = graph.edges_cnt()
 
                     reconstructed_graph = emb_m.reconstruct(num_links)
-                    print("#nodes = ", reconstructed_graph.nodes_cnt())
-                    print("#edges = ", reconstructed_graph.edges_cnt())
+                    # print("#nodes = ", reconstructed_graph.nodes_cnt())
+                    # print("#edges = ", reconstructed_graph.edges_cnt())
 
                     # graph reconstruction evaluation
                     precision_val = graph.link_precision(reconstructed_graph)
                     map_val, _ = graph.map_value(reconstructed_graph)
                     recall_val, _ = graph.recall(reconstructed_graph)
                     f1_val = (2 * precision_val * recall_val) / (precision_val + recall_val)
-
+                    print("PRECISION@K = ", precision_val, "MAP = ", map_val,
+                          ", RECALL = ", recall_val, ', F1 = ', f1_val)
                     with open('./cora_ml_dir.csv', 'a') as file:
                         file.write('{},{},{}.{}\n'.format(precision_val, map_val, recall_val, f1_val))
 
