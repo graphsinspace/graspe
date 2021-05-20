cb9f426a1203ff4394e46109d537e0a97cac28b3
nmilosev <nmilosevnm@gmail.com>
2021-05-20 14:16:29 +0200

    GCN Re-embedding
    - dodat test MLE-LID za GCN

--- src/graspe/tests/test_gcn.py
+++ src/graspe/tests/test_gcn.py
@@ -1,5 +1,6 @@
 from common.dataset_pool import DatasetPool
 from embeddings.embedding_gcn import GCNEmbedding
+from evaluation.lid_eval import LIDMLEEstimator, EmbLIDMLEEstimator
 
 
 def test_gcn_citeseer():
@@ -11,6 +12,24 @@ def test_gcn_citeseer():
     assert e._embedding is not None
     for i in range(34):
         print(i, e[i])
+
+
+def test_gcn_lid():
+    g = DatasetPool.load("karate_club_graph")
+    e = GCNEmbedding(
+        g, d=100, epochs=1, lr=0.05, layer_configuration=(128, 256, 128), act_fn="tanh"
+    )
+    e.embed()
+    assert e._embedding is not None
+
+    lid = EmbLIDMLEEstimator(g, e, 20)
+    lid.estimate_lids()
+    print("lid values", lid.lid_values)
+    print("avg", lid.get_avg_lid())
+    print("max", lid.get_max_lid())
+    print("min", lid.get_min_lid())
+    print("stdev", lid.get_stdev_lid())
+    print("sum", sum(lid.get_lid_values().values()))
 
 
 def test_gcn_all():
@@ -32,5 +51,6 @@ def test_gcn_all():
 
 
 if __name__ == "__main__":
-    test_gcn_citeseer()
+    test_gcn_lid()
+    # test_gcn_citeseer()
     # test_gcn_all()
