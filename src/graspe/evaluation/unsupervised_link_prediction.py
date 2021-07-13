ca2dc5479778df21a4469f25f9f236bcfeda3063
LucyK243 <lucy@dmi.uns.ac.rs>
2021-07-13 12:15:27 +0200

    unsupervised link prediciton for multiple embeddings

--- src/graspe/evaluation/unsupervised_link_prediction.py
+++ src/graspe/evaluation/unsupervised_link_prediction.py
@@ -1,9 +1,19 @@
 from common.graph import Graph
+from common.dataset_pool import DatasetPool
+from embeddings.embedding_node2vec import Node2VecEmbedding
+from embeddings.base.embedding import Embedding
+
+from multiprocessing import Process
+import sys
+from os import listdir
+from os.path import isfile, join
 import random as random
 from random import sample
-from embeddings.embedding_node2vec import Node2VecEmbedding
 import numpy as np
 import heapq as hq
+
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import accuracy_score
 
 
 class DistanceNodesPair:
@@ -37,10 +47,11 @@ class DistanceNodesPair:
 
 
 class UnsupervisedLinkPrediction:
-    def __init__(self, graph, h, embedding_method):
+    def __init__(self, graph, h, embedding, dataset):
         self._graph = graph
         self._h = h
-        self._embedding_method = embedding_method
+        self._embedding = embedding
+        self._dataset = dataset
         self.__predict()
 
     def __predict(self):
@@ -71,8 +82,8 @@ class UnsupervisedLinkPrediction:
             if edge not in self._hidden_edges:
                 self._newgraph.add_edge(edge[0], edge[1])
 
-        self._newembedding = Node2VecEmbedding(self._newgraph, 10, 0.1, 0.5, seed=42)
-        self._newembedding.embed()
+        #self._newembedding = Node2VecEmbedding(self._newgraph, 10, 0.1, 0.5, seed=42)
+        #self._newembedding.embed()
 
         self._dists = []
         for i in range(len(nodeset)):
@@ -82,8 +93,8 @@ class UnsupervisedLinkPrediction:
                 e = (node1[0], node2[0])
                 if e not in self._newgraph.edges():
                     d = np.linalg.norm(
-                        self._newembedding._embedding[node1[0]]
-                        - self._newembedding._embedding[node2[0]]
+                        self._embedding._embedding[node1[0]]
+                        - self._embedding._embedding[node2[0]]
                     )
                     hq.heappush(self._dists, DistanceNodesPair(d, (node1[0], node2[0])))
 
@@ -132,3 +143,73 @@ class UnsupervisedLinkPrediction:
                 map_sum += score
 
         return map_sum / float(relevant_nodes)
+
+    def get_jaccard_index(self):
+        return jaccard_score(self._hidden_edges, self._prediction)
+
+    def get_accuracy_score(self):
+        return accuracy_score(self._hidden_edges, self._prediction)
+
+
+    def eval(self):
+        outf = open("unsupervised-linkpred-eval-" + self.dataset_name + ".csv", "w")
+        outf.write("DATASET,DIM,precision@k,map,jaccard,accuracy\n")
+            
+        pAtK = self.get_precision_at_k(20)
+        jaccard = self.get_jaccard_index()
+        mapSc = self.get_map(20)
+        accuracy = self.get_accuracy_score()
+                    
+        msg = self._dataset + "," + "," + str(pAtK) + "," + str(mapSc) + "," +\
+            str(jaccard) + "," + str(accuracy)
+        outf.write(msg + "\n")
+
+        outf.close()
+
+datasets = [
+    #"karate_club_graph",
+    #"cora_ml",  
+    #"citeseer",
+    #"amazon_electronics_photo",
+    #"amazon_electronics_computers",
+    #"pubmed",
+    #"cora",
+    #"dblp"
+    "blog-catalog-undirected",
+    "ca-AstroPh-undirected",
+    "ca-CondMat-undirected",
+    "ca-GrQc-undirected",
+    "ca-HepPh-undirected",
+    "cit-HepPh",
+    "cit-HepTh",
+    "facebook-ego-undirected"
+]
+
+def link_pred_eval_function(d, folder):
+    graph = DatasetPool.load(d)
+    graph.remove_selfloop_edges()
+    graph = graph.to_undirected()
+
+    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+    for file_name in files:
+        if d in file_name:
+            emb = Embedding.from_file(file_name)
+            ds_name = d.split('/')[-1].split('.')[0]
+            ulp = UnsupervisedLinkPrediction(graph, 20, emb, ds_name)
+            ulp.eval()
+    
+
+
+if __name__ == "__main__":
+    folder = sys.argv[1]
+
+    procs = []
+    for d in datasets:
+        proc = Process(target=link_pred_eval_function, args=(d, folder))
+        procs.append(proc)
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
