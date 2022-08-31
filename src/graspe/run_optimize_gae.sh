b6218e5b0d1afefa9e0e7226b5004634fef62cfe
Dusan Stamenkovic <dusan.v.stamenkovic@icloud.com>
2022-08-31 17:34:50 +0200

    optimize gae sbatch script

---
+++ src/graspe/run_optimize_gae.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=GAEOPT
+#SBATCH --partition=cuda
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --nodelist=n03
+#SBATCH --time=23-20:00:00
+#SBATCH --output /home/stamendu/graspe/src/graspe/slurm.%J.out
+#SBATCH --error /home/stamendu/graspe/src/graspe/slurm.%J.err
+#SBATCH --export=ALL
+srun /home/stamendu/miniconda3/envs/graspe/bin/python /home/stamendu/graspe/src/graspe/optimize_gae.py
